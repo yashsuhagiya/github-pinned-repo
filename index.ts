31172bb221c27d2c627088edd70a8025843321db
Yash Suhagiya <yashsuhagiya401@gmail.com>
2024-09-04 10:47:52 +0200

    chore: Update file path for index.html in home page route

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
 import getPinnedRepo from "./utils/getPinnedRepo.js";
+import { readFile } from "fs/promises";
+import path from "path";
 
 const app = new Hono();
 const port = process.env.PORT || 3000;
@@ -10,9 +12,13 @@ app.use("/*", serveStatic({ root: "./public" }));
 
 // Home page
 app.get("/", async (c) => {
-  const file = Bun.file("./public/index.html");
-  const content = await file.text();
-  return c.html(content);
+  const filePath = path.join(__dirname, "public", "index.html");
+  try {
+    const content = await readFile(filePath, "utf-8");
+    return c.html(content);
+  } catch (error) {
+    return c.text("File not found", 404);
+  }
 });
 
 // Route to handle the username-specific API
